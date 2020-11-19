55f62346605cb454c343fd573c172722a0c7f5c6
Federico Ariel Castagnini <fede@191161.xyz>
2020-11-19 10:31:18 -0800

    fixing lint error

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -6,13 +6,14 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")
 
 from audiobook.helpers import Audiobook
 
+AUDIOBOOK1 = "~/tests/fixture/audiobook1.m4a"
 
 class HelpersTestSuite(unittest.TestCase):
     """Testing the helper objects."""
 
     def test_audiobook_class(self):
         """Testing that the Audiobook class can load a template audiobook"""
-        audiobook1_absolute_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "~/tests/fixture/audiobook1.m4a"))
+        audiobook1_absolute_path = os.path.abspath(os.path.join(os.path.dirname(__file__), AUDIOBOOK1))
         audiobook1 = Audiobook(audiobook1_absolute_path)
         assert audiobook1.audiobook_file == audiobook1_absolute_path
 
